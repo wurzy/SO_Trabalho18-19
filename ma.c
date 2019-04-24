727b330f353d9dbd5bedc37afca13aa8883a45ba
luk <ojosepedro@gmail.com>
2019-04-24 22:06:07 +0100

    deprecated now has identifier

--- ma.c
+++ ma.c
@@ -17,7 +17,7 @@ off_t insert_strings(char *nome){
 }
 
 void insert_artigo(char *nome, char *preco){
-  char buf[ARTIGO_LENG +1];
+  char buf[50];
   int fd = open("artigos", O_CREAT | O_RDWR, 0777);
   off_t s = lseek(fd, 0, SEEK_END);
   off_t offset = insert_strings(nome);
@@ -31,29 +31,32 @@ void insert_artigo(char *nome, char *preco){
 void alteraNome(char *codigo, char *nome){
   long int num = atol(codigo);
   off_t offset = num * (ARTIGO_LENG +1);
+
   int fd1 = open("artigos", O_RDWR, 0777);
   int fd2 = open("strings", O_RDWR, 0777);
   int fd3 = open("deprecated", O_CREAT | O_RDWR, 0777);
+
   lseek(fd1, offset, SEEK_SET);
 
   char buf[100]; // Perceber o pq de só dar com 100
   read(fd1, &buf, ARTIGO_LENG);
-  char *field = strndup(buf + 9, 8); // 9 = size(numero) + " "
+
+  char *identifier = strndup(buf, NUMBER_LEN_I);
+  char *field = strndup(buf + NUMBER_LEN_I + 1, POINTER_LEN_I); // 9 = size(numero) + " "
 
   char name[100];
+  long offset2 = atol(field);
   sprintf(name, "%s\n",nome);
-  int offset2 = atol(field);
   lseek(fd2, offset2, SEEK_SET);
 
-  char lin[100];
-  ssize_t size;
-  size = readln(fd2, lin, 100);
-  sprintf(buf, "%s %s\n", field, lin);
+  char lin[100], buffer[100];
+  ssize_t size = readln(fd2, lin, 100);
+  printf("size = %ld, str = %s", size, lin);
+  snprintf(buffer, POINTER_LEN_I + 1 + NUMBER_LEN_I + 1 + size + 1, "%s %s %s\n", field, identifier, lin);
   lseek(fd3, 0, SEEK_END);
-  write(fd3, buf, 9 + size - 1);
+  write(fd3, buffer, POINTER_LEN_I + 1 + NUMBER_LEN_I + 1 + size + 1);
 
   offset2 = lseek(fd2, 0, SEEK_END);
-  printf("%ld\n", offset);
   write(fd2, name, strlen(name));
 
   sprintf(buf, POINTER_SIZE, offset2);
